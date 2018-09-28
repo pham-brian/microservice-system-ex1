38e49b55181abade05f5d63dd9d0bad71fd62ec1
hieu_pham_vn <xuanhieu.pham@ascendcorp.com>
2018-09-28 11:02:30 +0700

    update poc

--- micro-service-1/src/main/java/com/hieupx/springcloudclient/SpringCloudClientApplication.java
+++ micro-service-1/src/main/java/com/hieupx/springcloudclient/SpringCloudClientApplication.java
@@ -1,12 +1,19 @@
 package com.hieupx.springcloudclient;
 
+import com.hieupx.springcloudclient.proxy.Service2Proxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class SpringCloudClientApplication {
 
@@ -19,11 +26,20 @@ public class SpringCloudClientApplication {
 @RestController
 class MessageRestController {
 
-	@Value("${branch}")
+	@Autowired
+	Service2Proxy service2Proxy;
+
+	@Value("${msg}")
 	private String msg;
 
 	@RequestMapping("/msg")
 	String getMsg() {
 		return this.msg;
 	}
+
+	@RequestMapping("/get/service2/msg")
+	String getService2Msg() {
+		return service2Proxy.getService2Msg();
+	}
+
 }
