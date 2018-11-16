b37c740a57a8f7299d198c6332390da535579038
hieu_pham_vn <xuanhieu.pham@ascendcorp.com>
2018-11-16 17:21:22 +0700

    add load balancer config

---
+++ micro-service-2-lb/src/main/java/com/hieupx/springcloudclient/Service2LB.java
@@ -0,0 +1,39 @@
+package com.hieupx.springcloudclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableFeignClients
+public class Service2LB {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service2LB.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${msg}")
+	private String msg;
+
+	@RequestMapping("/msg")
+	String getMsg() {
+		return this.msg;
+	}
+
+	@RequestMapping("/get/exception")
+	public String getHandleException() throws Exception {
+		System.out.println("Throws exception for tracing demo");
+		throw new Exception();
+	}
+}
